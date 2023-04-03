85e81ad5b2189a9dde2e8414c27992a4b25d22fd
elif c <86270311+elif-c@users.noreply.github.com>
2023-04-03 21:59:26 +0300

    First commit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-
+while True:
+    text = input("Enter text to see character count: ")
+    mode = input("All characters or ignore whitespaces? (all/ignore/exit): ")
+    match mode:
+        case "all":
+            print(f"There's {len(text.strip())} characters.")
+        case "ignore":
+            text = text.strip().replace(" ", "")
+            print(text)
+            print(f"There's {len(text)} characters without whitespaces.")
+        case "exit":
+            break
+        case _:
+            print("Try again.")
+            continue
+print("Bye ^^")
+exit()
